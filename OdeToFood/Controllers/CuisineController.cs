5acbab636621b26beff42655091cc19b6be410cf
Simon Zhang <simon861102@gmail.com>
2019-06-04 21:45:23 +1000

    first commit

---
+++ OdeToFood/Controllers/CuisineController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class CuisineController : Controller
+    {
+        public ActionResult Search(string name = "French")
+        {
+            var message = Server.HtmlEncode(name);
+            return Json(new { Nationality = message, Name = "Simon" }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var controller = RouteData.Values["controller"];
+            var action = RouteData.Values["action"];
+            var id = RouteData.Values["id"];
+            var message = String.Format("{0}::{1} {2}",controller, action, id);
+            ViewBag.Message = message;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            var model = new AboutModel();
+            model.Name = "Simon";
+            model.Location = "LA, USA";
+
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
